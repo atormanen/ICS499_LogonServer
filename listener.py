55b842d9bd32a253009e2e56562cab875173bb13
Asher <TormanenAsher@gmailc.om>
2020-02-01 17:49:32 -0600

    auto ip settings

--- listener.py
+++ listener.py
@@ -5,11 +5,22 @@ import threading
 import json
 from processRequest import *
 
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 hostname = socket.gethostname()
-#serverIp = '10.0.0.243'
-#serverIp = '192.168.1.174'
+serverIp = get_ip()
 portNumber = 12345
 bufferSize = 1024
 processReq = processRequest()
