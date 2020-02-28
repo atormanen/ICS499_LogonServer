d4760ceaac498a8006702f6414f5909d61c3b547
unknown <tormanenasher@gmail.com>
2020-02-28 07:22:56 -0600

    response syntax created

--- userManagement/Signin.py
+++ userManagement/Signin.py
@@ -1,11 +1,11 @@
 #Signin will handle the mechanics of signing a user in
 class Signin:
 
-    def __init__(self, mysqlDB):
-        self.mysqlDB = mysqlDB
+    def __init__(self, db):
+        self.db = db
 
     def validatePassword(self, username, password):
-        dbPassword = self.mysqlDB.getPasswordFor(username)
+        dbPassword = self.db.getPasswordFor(username)
         dbPassword = dbPassword[0][0]
 		#compare password to given getPassword
         if(password == dbPassword):
@@ -15,7 +15,10 @@ class Signin:
     def signin(self, parsedData):
         username = parsedData["username"]
         password = parsedData["password"]
-        if(compareResult == self.validatePassword(username, password)):
+        if(self.validatePassword(username, password)):
             #do the signin stuff
+            #Generate a session token
+            #Set a token expiration in the db
+            #Bundle the tocken into the response package
             return True
         return False
