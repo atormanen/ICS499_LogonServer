0ddc57e950f8c0c8e40a3b0c16bc77fd573533a0
Asher <TormanenAsher@gmailc.om>
2020-03-22 14:07:50 -0500

    test

--- ProcessRequest.py
+++ ProcessRequest.py
@@ -36,7 +36,7 @@ class ProcessRequest:
 
         if parsedData["requestType"] == "signin":
             token = self.signin.signin(parsedData)
-            print("Signin" + token)
+            print("Signin" + str(token))
             print(parsedData)
             reqItem.signinResponse(token)
             self.responder.sendResponse(reqItem)
