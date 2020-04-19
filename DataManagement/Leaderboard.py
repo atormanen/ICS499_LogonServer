d1750eb172e58d75fc279d0ac63a75c36a28626a
Asher <TormanenAsher@gmailc.om>
2020-04-19 15:57:07 -0500

    test

--- DataManagement/Leaderboard.py
+++ DataManagement/Leaderboard.py
@@ -18,6 +18,28 @@ class Leaderboard:
 
         print(type(resp))
 
-
+        for item in resp:
+            i = 0
+            user = {
+                    "username":"",
+                    "user_id":"",
+                    "games_played":"",
+                    "games_won":"",
+                    "games_resigned":"",
+                    "score":"",
+                    "longest_win_streak":"",
+                    "shortest_game":""
+            }
+            user["username"] = resp[i][0]
+            user["user_id"] = resp[i][1]
+            user["games_played"] = resp[i][2]
+            user["games_won"] = resp[i][3]
+            user["games_resigned"] = resp[i][4]
+            user["score"] = resp[i][5]
+            user["longest_win_streak"] = resp[i][6]
+            user["shortest_game"] = resp[i][7]
+            responseObj = json.dumps(user)
+            i = i + 1
+        print(responseObj)
 
         reqItem.mostChessGamesWonResponse(numberOfGames, resp)
