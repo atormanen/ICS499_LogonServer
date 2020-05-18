c25dfee16e3363a76f7b509675726809c87cc50e
Joshua Zierman <py1422xs@metrostate.edu>
2020-05-18 13:38:11 -0500

    changes to print lines

--- database/MysqlDB.py
+++ database/MysqlDB.py
@@ -19,7 +19,7 @@ class MysqlDB:
 
     def changePassword(self, username, password):
         querry = "UPDATE user SET password = '" + str(password) + "' WHERE username = '" + str(username) +"';"
-        print(querry)
+        print("querry:",querry)
         return querry
 
     def validateUserExists(self,username):
@@ -30,7 +30,7 @@ class MysqlDB:
     def checkIfFriendRequestExists(self, userId, friendsId):
         querry = "SELECT EXISTS(SELECT user_id FROM friend_list WHERE user_id = \
         " + str(userId) + " AND friend_id = " + str(friendsId) + " AND request_accepted = 0);"
-        print(querry)
+        print("querry:",querry)
         return querry
 
     def validateUsernameAvailable(self,username):
@@ -78,7 +78,7 @@ class MysqlDB:
         querry = "UPDATE user SET token_creation='"+tokenExpiration + \
                     "',signon_token='" + token + "' WHERE username = " + \
                     "'" + username + "';"
-        print(querry)
+        print("querry:",querry)
         return querry
 
     def getToken(self,username):
@@ -97,7 +97,7 @@ class MysqlDB:
     def addFriend(self, userId, friendId):
         querry = "INSERT INTO friend_list VALUES(" + str(userId) +\
                     ","+ str(friendId) + ",1);"
-        print(querry)
+        print("querry:",querry)
         return querry
 
     #UserId and friendId are backwards... that is intentional
@@ -105,12 +105,12 @@ class MysqlDB:
         querry = "UPDATE friend_list set request_accepted = " + str(acceptedRequest) +\
                     " WHERE friend_id = " + str(userId) + " AND user_id = " +\
                     str(friendId) + ";"
-        print(querry)
+        print("querry:",querry)
         return querry
 
     def removeFriend(self, userId, friendId):
         querry = "DELETE FROM friend_list WHERE user_id = " + str(userId) + " AND friend_id = " + str(friendId) + ";"
-        print(querry)
+        print("querry:",querry)
         return querry
 
     def checkForFriendRequests(self, userId):
@@ -120,25 +120,25 @@ class MysqlDB:
         on user.user_id = friend_list.user_id\
         where friend_list.friend_id = " + str(userId) + \
         " AND request_accepted = 0;"
-        print(querry)
+        print("querry:",querry)
         return querry
 
     def logout(self, username):
         querry = "UPDATE user SET signon_token = 'null' WHERE username = '" + \
                 username + "';"
-        print(querry)
+        print("querry:",querry)
         return querry
 
     def getMostGamesWon(self, numberOfGames):
         querry = "select user.username, user_statistics.* from user inner join user_statistics on user.user_id = user_statistics.user_id order by games_won desc limit " + str(numberOfGames) + ";"
-        print(querry)
+        print("querry:",querry)
         return querry
 
     def getLongestWinStreak(self, numberOfGames):
         querry = "select user.username, user_statistics.* from user \
         inner join user_statistics on user.user_id = user_statistics.user_id\
         order by longetst_win_streak desc limit" + str(numberOfGames) + ";"
-        print(querry)
+        print("querry:",querry)
         return querry
 
     def getAccountInfo(self, username):
@@ -146,14 +146,14 @@ class MysqlDB:
         user.match_clock_choice, user.automatic_queening, user.disable_pausing, user.require_commit_press, user_statistics.level FROM user \
         inner join user_statistics on user.user_id = user_statistics.user_id \
         Where username = '" + str(username) + "';"
-        print(querry)
+        print("querry:",querry)
         return querry
 
     def saveAccountInfo(self, username, data):
         querry = "UPDATE user, user_statistics SET user.avatar = " + str(data["avatarStyle"]) + ", user.chess_board_style = " + str(data["chessboardStyle"]) + ", user.chess_piece_style = " + str(data["chesspieceStyle"]) +\
         ", user.match_clock_choice =  " + str(data["matchClockChoice"]) + ", user.automatic_queening = " + str(data["automaticQueening"]) + ", user.disable_pausing = " + str(data["disablePausing"]) +\
         ", user.require_commit_press =  " + str(data["requireCommitPress"]) + ", user_statistics.level = " + str(data["level"]) + " WHERE user.username = '" + str(username) + "';"
-        print(querry)
+        print("querry:",querry)
         return querry
 
     def saveAccountInfoByKey(self, username, key, value):
@@ -162,7 +162,7 @@ class MysqlDB:
         if(column is None):
             return None
         querry = "UPDATE user, user_statistics SET " + str(column) + " = " + str(value) + " WHERE user.username = '" + str(username) + "';"
-        print(querry)
+        print("querry:",querry)
         return querry
 
     def getColumn(self, key):
