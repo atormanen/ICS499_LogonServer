83102a847ab477e7a1b70ad11a4fd62047cafe96
Asher <TormanenAsher@gmail.com>
2021-09-27 19:51:11 -0500

    test

--- global_logger.py
+++ global_logger.py
@@ -9,7 +9,8 @@ log_formatter = logging.Formatter("%(levelname)-7.7s %(threadName)-12.12s %(asct
 logger = logging.getLogger()
 logging.addLevelName(VERBOSE, verbose_level)
 
-file_handler = logging.FileHandler("{0}/{1}.log".format(logPath, fileName))
+# file_handler = logging.FileHandler("{0}/{1}.log".format(logPath, fileName))
+file_handler = logging.handlers.WatchedFileHandler(os.environ.get("LOGFILE", "./logs/logon_server.log"))
 file_handler.setFormatter(log_formatter)
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(log_formatter)
