67b026cc55d39f304b6b21750e89936ba4db199a
Asher <TormanenAsher@gmail.com>
2021-10-05 21:10:33 -0500

    null

--- database/MysqlDB.py
+++ database/MysqlDB.py
@@ -146,7 +146,7 @@ class MysqlDB:
 
     def logout(self, username):
         self.log_function_name()
-        querry = f"UPDATE user SET signon_token = {None} WHERE username = '{username}';"
+        querry = f"UPDATE user SET signon_token = null WHERE username = '{username}';"
         return querry
 
     def getMostGamesWon(self, numberOfGames):
