6045b9e84708b99f49e3d950a4e9e52105850233
Joshua Zierman <github@zierman.us>
2021-11-10 12:43:43 -0600

    checking pyscript

--- tmp.py
+++ tmp.py
@@ -1,46 +1,150 @@
 #!/usr/bin/env python3
 import subprocess
+import sys
 from subprocess import run
 
 if __name__ == '__main__':
+
+    width = 20
+    verbose = True
+    quiet = False
+    should_monitor_log = True
+    should_show_service_status = True
+    operation = 'pull'
+
+    c_operation = operation.strip().lower()
+
+
+    def qprint(*args, **kwargs):
+        print(*args, **kwargs)
+
+
+    def vprint(*args, **kwargs):
+        if verbose:
+            print(*args, **kwargs)
+
+
+    def vnprint(*args, **kwargs):
+        if not quiet:
+            print(*args, **kwargs)
+
+
+    def nprint(*args, **kwargs):
+        if not verbose and not quiet:
+            print(*args, **kwargs)
+
 
     print('start')
     process = run(['id', '-u'], check=True, text=True, capture_output=True)
     uid = str(process.stdout)
     if int(uid) != 0:
-        print("Unauthorized - Try running with sudo.")
+        qprint("Unauthorized - Try running with sudo.")
         exit(1)
 
-    print('---------------------------------------')
-    run(['systemctl', 'stop', 'jar_logon.service'], check=True, text=True)
+    # --------------------------------------------
+    #               Stopping Service
+    vnprint('-' * width)
+    vnprint('Stopping Service'.center(width))
+    process = run(['systemctl', 'stop', 'jar_logon.service'], check=True, text=True, capture_output=True)
+    vprint(process.stdout)
+    vprint(process.stderr, file=sys.stderr)
+    nprint('')
 
+    # --------------------------------------------
+    #                  Clearing Logs
+    vnprint('-' * width)
+    vnprint('Clearing Logs'.center(width))
     with open('./logs/logon_server.log', 'w'):
         ...  # clear the log
 
-    print('---------------------------------------')
+    # --------------------------------------------
+    #               Performing Git Stash
+    vnprint('-' * width)
+    vnprint('Performing Git Stash'.center(width))
     run(['git', 'stash'], check=True, text=True)
-    print('---------------------------------------')
-    run(['git', 'fetch'], check=True, text=True)
-    print('---------------------------------------')
-    run(['git', 'pull'], check=True, text=True)
-    print('---------------------------------------')
-    run(['chown', '-R', 'jar_user:jar_user', '.'],  text=True)
+    vprint(process.stdout)
+    vprint(process.stderr, file=sys.stderr)
 
+    # --------------------------------------------
+    #               Performing Git Fetch
+    vnprint('-' * width)
+    vnprint('Performing Git Fetch'.center(width))
+    run(['git', 'fetch'], check=True, text=True)
+    vprint(process.stdout)
+    vprint(process.stderr, file=sys.stderr)
+
+    # --------------------------------------------
+    #               Performing Git <op>
+    vnprint('-' * width)
+    vnprint(f'Performing Git {operation.capitalize()}'.center(width))
+    run(['git', str(operation)], check=True, text=True)
+    vprint(process.stdout)
+    vprint(process.stderr, file=sys.stderr)
+
+    # --------------------------------------------
+    #               Changing Ownership
+    vnprint('-' * width)
+    vnprint('Changing Ownership'.center(width))
+    run(['chown', '-R', 'jar_user:jar_user', '.'], text=True)
+    vprint(process.stdout)
+    vprint(process.stderr, file=sys.stderr)
+
+    # --------------------------------------------
+    #           Making Scripts Executable
+    vnprint('-' * width)
+    vnprint('Making Scripts Executable'.center(width))
     process = run("grep -rl '^#!/.*' .", text=True, capture_output=True, shell=True)
     lines = str(process.stdout).strip().split('\n')
     if lines and not lines[0].startswith('grep:'):
         for item in lines:
             if '/.git/' not in item:
                 run(['chmod', '+x', item], check=True, text=True)
+                vprint(process.stdout)
+                vprint(process.stderr, file=sys.stderr)
 
-    # run(['chmod', '0554', '-R', '.'], check=True, text=True, shell=True)
+    # --------------------------------------------
+    #           Changing Mode Bits
+    vnprint('-' * width)
+    vnprint('Changing Mode Bits'.center(width))
     run('chmod 0554 -R .', check=True, text=True, shell=True, capture_output=True)
+    vprint(process.stdout)
+    vprint(process.stderr, file=sys.stderr)
     run(['chmod', '0664', './logs/logon_server.log'], check=True, text=True, capture_output=True)
+    vprint(process.stdout)
+    vprint(process.stderr, file=sys.stderr)
     run(['chmod', '0440', './params.json'], check=True, text=True, capture_output=True)
-    run(['systemctl', 'start', 'jar_logon.service'], check=True, text=True, capture_output=True)
-    # process = run(['systemctl', 'status', '-n', '100', 'jar_logon.service'], check=True, text=True,
-    #               stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    # status_output = str(process.stdout)
-    # print(status_output)
+    vprint(process.stdout)
+    vprint(process.stderr, file=sys.stderr)
 
-    print('done')
+    # --------------------------------------------
+    #           Restarting Service
+    vnprint('-' * width)
+    vnprint('Restarting Service'.center(width))
+    run(['systemctl', 'restart', 'jar_logon.service'], check=True, text=True, capture_output=True)
+    vprint(process.stdout)
+    vprint(process.stderr, file=sys.stderr)
+    
+    if(should_show_service_status):
+        # --------------------------------------------
+        #           Service Status
+        vnprint('-' * width)
+        vnprint('Starting Log Monitor'.center(width))
+        process = run(['systemctl', 'status', '-n', '100', 'jar_logon.service'], check=True, text=True,
+                      stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        status_output = str(process.stdout)
+        qprint(status_output)
+
+
+    if(should_monitor_log):
+        # --------------------------------------------
+        #           Starting Log Monitor
+        vnprint('-' * width)
+        vnprint('Starting Log Monitor'.center(width))
+
+        t = run(['tail', '-n', '100', '-f', './logs/logon_server.log'], text=True)
+    else:
+        vnprint('-' * width)
+        vnprint('-' * width)
+        
+        
+
