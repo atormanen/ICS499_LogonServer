609ca76c1db931fc4e7c869cc2ba42f080204ae4
Joshua Zierman <github@zierman.us>
2021-11-10 00:51:15 -0600

    bug hunt

--- controller.py
+++ controller.py
@@ -86,12 +86,17 @@ class Controller:
         try:
             req = RequestProcessor(self, self.request_queue, REQUEST_PROCESSOR_TIMEOUT_SECONDS)
             req.process_requests()
-        except RuntimeError as e:
+            retries = 0
+        except RequestProcessor as e:
             if retries < allowed_retries:
                 retries += 1
             else:
                 self.error = e
                 self.should_stay_alive = False
+        except BaseException as e:
+            self.error = e
+            self.should_stay_alive = False
+
 
     @logged_method
     def create_request_processors(self):
