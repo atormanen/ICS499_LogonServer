f645649f4c2b7001f94cae161d5e84e569d761c6
Joshua Zierman <github@zierman.us>
2021-11-20 02:19:56 -0600

    made abstract methods raise NotImplementedError

--- util/threading.py
+++ util/threading.py
@@ -7,23 +7,23 @@ class ThreadController(Protocol):
     @property
     @abstractmethod
     def error(self) -> BaseException:
-        ...
+        raise NotImplementedError
 
     @error.setter
     @abstractmethod
     def error(self, value):
-        ...
+        raise NotImplementedError
 
     @property
     @abstractmethod
     def should_stay_alive(self):
-        ...
+        raise NotImplementedError
 
     @should_stay_alive.setter
     @abstractmethod
     def should_stay_alive(self, value):
-        ...
+        raise NotImplementedError
 
     @abstractmethod
     def _wait_until_no_longer_should_stay_alive(self):
-        ...
+        raise NotImplementedError
